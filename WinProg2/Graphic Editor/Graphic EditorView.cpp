e58656889f5b2de4b5ff125df919711134a92102
hj_tl22@live.co.kr <hj_tl22@live.co.kr>
2015-05-10 19:58:17 +0900

    초기 생성

---
+++ WinProg2/Graphic Editor/Graphic EditorView.cpp
@@ -0,0 +1,137 @@
+// 이 MFC 샘플 소스 코드는 MFC Microsoft Office Fluent 사용자 인터페이스("Fluent UI")를 
+// 사용하는 방법을 보여 주며, MFC C++ 라이브러리 소프트웨어에 포함된 
+// Microsoft Foundation Classes Reference 및 관련 전자 문서에 대해 
+// 추가적으로 제공되는 내용입니다.  
+// Fluent UI를 복사, 사용 또는 배포하는 데 대한 사용 약관은 별도로 제공됩니다.  
+// Fluent UI 라이선싱 프로그램에 대한 자세한 내용은 
+// http://go.microsoft.com/fwlink/?LinkId=238214.
+//
+// Copyright (C) Microsoft Corporation
+// All rights reserved.
+
+// Graphic EditorView.cpp : CGraphicEditorView 클래스의 구현
+//
+
+#include "stdafx.h"
+// SHARED_HANDLERS는 미리 보기, 축소판 그림 및 검색 필터 처리기를 구현하는 ATL 프로젝트에서 정의할 수 있으며
+// 해당 프로젝트와 문서 코드를 공유하도록 해 줍니다.
+#ifndef SHARED_HANDLERS
+#include "Graphic Editor.h"
+#endif
+
+#include "Graphic EditorDoc.h"
+#include "Graphic EditorView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CGraphicEditorView
+
+IMPLEMENT_DYNCREATE(CGraphicEditorView, CView)
+
+BEGIN_MESSAGE_MAP(CGraphicEditorView, CView)
+	// 표준 인쇄 명령입니다.
+	ON_COMMAND(ID_FILE_PRINT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_DIRECT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_PREVIEW, &CGraphicEditorView::OnFilePrintPreview)
+	ON_WM_CONTEXTMENU()
+	ON_WM_RBUTTONUP()
+END_MESSAGE_MAP()
+
+// CGraphicEditorView 생성/소멸
+
+CGraphicEditorView::CGraphicEditorView()
+{
+	// TODO: 여기에 생성 코드를 추가합니다.
+
+}
+
+CGraphicEditorView::~CGraphicEditorView()
+{
+}
+
+BOOL CGraphicEditorView::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: CREATESTRUCT cs를 수정하여 여기에서
+	//  Window 클래스 또는 스타일을 수정합니다.
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CGraphicEditorView 그리기
+
+void CGraphicEditorView::OnDraw(CDC* /*pDC*/)
+{
+	CGraphicEditorDoc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: 여기에 원시 데이터에 대한 그리기 코드를 추가합니다.
+}
+
+
+// CGraphicEditorView 인쇄
+
+
+void CGraphicEditorView::OnFilePrintPreview()
+{
+#ifndef SHARED_HANDLERS
+	AFXPrintPreview(this);
+#endif
+}
+
+BOOL CGraphicEditorView::OnPreparePrinting(CPrintInfo* pInfo)
+{
+	// 기본적인 준비
+	return DoPreparePrinting(pInfo);
+}
+
+void CGraphicEditorView::OnBeginPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 인쇄하기 전에 추가 초기화 작업을 추가합니다.
+}
+
+void CGraphicEditorView::OnEndPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 인쇄 후 정리 작업을 추가합니다.
+}
+
+void CGraphicEditorView::OnRButtonUp(UINT /* nFlags */, CPoint point)
+{
+	ClientToScreen(&point);
+	OnContextMenu(this, point);
+}
+
+void CGraphicEditorView::OnContextMenu(CWnd* /* pWnd */, CPoint point)
+{
+#ifndef SHARED_HANDLERS
+	theApp.GetContextMenuManager()->ShowPopupMenu(IDR_POPUP_EDIT, point.x, point.y, this, TRUE);
+#endif
+}
+
+
+// CGraphicEditorView 진단
+
+#ifdef _DEBUG
+void CGraphicEditorView::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CGraphicEditorView::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CGraphicEditorDoc* CGraphicEditorView::GetDocument() const // 디버그되지 않은 버전은 인라인으로 지정됩니다.
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CGraphicEditorDoc)));
+	return (CGraphicEditorDoc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CGraphicEditorView 메시지 처리기
