b1d2ec918813020ee105a8fe686d884dbc594392
성민지 <sungmj28@naver.com>
2015-05-16 20:56:13 +0900

    CText 수정00

---
+++ WinProg2/Graphic Editor/Text.cpp
@@ -0,0 +1,35 @@
+#include "stdafx.h"
+#include "Text.h"
+
+
+CText::CText(){
+}
+
+
+///////////////////////////  Defalt 속성  //////////////////////////////////
+
+void CText::FontDisplay(){//related information Derived from CFigureSetting
+}
+////////////////////////////////////////////////////////////////////////////
+
+void CText::FontTransform( ){//Font transformation when setting values are changed
+
+}
+
+void CText::SizeTransform(){//Size transformation when when setting values are changed ex) click event
+
+}
+/////////////////
+
+void  CText::FontDestroy(){// 제거
+
+}
+
+CText::~CText(){//delete
+}
+
+//CString m_FontName;
+//int m_FontSize;
+//BOOL m_FontBold;
+//BOOL m_FontItalic;
+//BOOL m_FontUnderline;
--- WinProg2/Graphic Editor/Text.h
+++ WinProg2/Graphic Editor/Text.h
@@ -8,9 +8,10 @@ public:
 	CText();
 	~CText();//소멸자
 	
+	virtual void FontDisplay();
 	virtual void FontTransform();// 폰트변형
 	virtual void SizeTransform();// 사이즈변경
-
+	virtual void FontDestroy();// 제거
 private:
 //	CPoint points;
 
