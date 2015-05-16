398dd7cbaf7c2a79760a826fb1356256bfa23b97
ByunJun <bjjang9407@naver.com>
2015-05-16 18:51:06 +0900

    Line 클래스 정의,선언 만드는 중1

--- WinProg2/Graphic Editor/Line.cpp
+++ WinProg2/Graphic Editor/Line.cpp
@@ -4,9 +4,52 @@
 
 CLine::CLine()
 {
+
 }
 
 
 CLine::~CLine()
 {
+
+}
+
+void CLine::Create(CPoint startingPoint)
+{
+	//나중에 상대 좌표를 사용할 꺼임
+	this->m_startingPoint = startingPoint;
+	m_PointsList.AddHead(CPoint(0,0));
+}
+
+// 속성에 관한 부분은 setting class를 이용하기로 했음
+void CLine::setLineColor(Color lineColor) {
+	m_lineColor = lineColor;
+}
+
+void CLine::setLineWidth(int lineWidth) {
+	 // m_lineWidth = lineWidth;
+}
+
+void CLine::setLinePattern(int linePattern) {
+	// m_linePattern = linePattern;
+}
+
+
+void CLine::move(CPoint target) {
+
+}
+
+void CLine::resize(CPoint point) {
+
+}
+
+void CLine::propResize(CPoint point) {
+
+}
+
+void CLine::destroy() {
+
+}
+
+CList<CPoint, CPoint&>& CLine::GetPointsList(){
+	return m_PointsList;
 }
--- WinProg2/Graphic Editor/Line.h
+++ WinProg2/Graphic Editor/Line.h
@@ -1,10 +1,47 @@
 #pragma once
 #include "Figure.h"
-class CLine :
-	public CFigure
+class CLine : public CFigure
 {
 public:
 	CLine();
 	~CLine();
+
+	/* 선 생성*/
+	virtual void Create(CPoint startingPoint);
+
+	/* 선 색 변경 */
+	virtual void setLineColor(Color lineColor);
+
+	/* 선 두께*/
+	virtual void setLineWidth(int lineWidth);
+
+	/* 선 패턴*/
+	virtual void setLinePattern(int linePattern);
+
+	/* 전체 이동*/
+	virtual void move(CPoint target);
+
+	/* 크기 변경 */
+	virtual void resize(CPoint point);
+
+	/* 정비례 크기*/
+	virtual void propResize(CPoint point);
+
+	/* 삭제 */
+	virtual void destroy();
+
+	/**/
+	CList<CPoint, CPoint&>& GetPointsList();
+
+private:
+
+	/* 두 개의 좌표 */
+	CList <CPoint, CPoint&> m_PointsList;
+
+	/* 그리기 위한 펜 */
+	CPen m_LinePen;
+
+
 };
 
+   
