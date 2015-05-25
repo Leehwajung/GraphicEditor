d679a2ce2d9b96c65b1e9dda6e2dcf84ee97c2b6
이화중 <hj_tl22@live.co.kr>
2015-05-25 20:57:58 +0900

    글로벌 클래스 추가

--- WinProg2/Graphic Editor/Shape.cpp
+++ WinProg2/Graphic Editor/Shape.cpp
@@ -1,6 +1,6 @@
 // Shape.cpp : CShape 클래스의 구현
 // CShape : 2차원 도형 개체를 정의하는 클래스
-// @Author Lee Hwajung
+// 
 
 #include "stdafx.h"
 #include "Graphic Editor.h"
@@ -10,6 +10,7 @@
 // CShape
 
 CShape::CShape()
+	: m_Pen(Pe)
 {
 }
 
