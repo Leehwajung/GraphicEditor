6f113d471d7698bfb376dabf1b026427eb768df6
성민지 <sungmj28@naver.com>
2015-05-25 21:06:42 +0900

    충돌 커밋

--- WinProg2/Graphic Editor/Shape.cpp
+++ WinProg2/Graphic Editor/Shape.cpp
@@ -1,6 +1,6 @@
 // Shape.cpp : CShape 클래스의 구현
 // CShape : 2차원 도형 개체를 정의하는 클래스
-// @Author Lee Hwajung 이하와중 니취팔로마
+// @Author Lee Hwajung 이하와중 니취팔로마gg
 #include "stdafx.h"
 #include "Graphic Editor.h"
 #include "Shape.h"
