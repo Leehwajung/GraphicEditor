975d308340d38cd9dc75efc5438e7c51fa5e958c
성민지 <sungmj28@naver.com>
2015-05-26 11:32:13 +0900

    Line set color width pattern 인자 변경

--- WinProg2/Graphic Editor/Line.h
+++ WinProg2/Graphic Editor/Line.h
@@ -35,9 +35,9 @@ public:
 	// Menu Item
 	virtual void destroy();										// 개체 삭제
 
-	virtual void setLineColor(const Color& LineColor);					// 선 색 설정
-	virtual void setLineWidth(const REAL& LineWidth);					// 선 두께 설정
-	virtual void setLinePattern(const DashStyle& LinePattern);				// 선 패턴 설정
+	virtual void setLineColor(const Color& LineColor);			// 선 색 설정
+	virtual void setLineWidth(const REAL& LineWidth);			// 선 두께 설정
+	virtual void setLinePattern(const DashStyle& LinePattern);	// 선 패턴 설정
 	virtual void setProperties(CFigureProperties properties);	// 설정된 값으로 개체 속성 설정
 
 private:
