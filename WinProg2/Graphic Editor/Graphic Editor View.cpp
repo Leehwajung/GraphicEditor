b36e0a0dac49c6a7aee2fe029f49793a47340cd1
성민지 <sungmj28@naver.com>
2015-06-08 08:55:04 +0900

    VK_DELETE하면 IsEmpty후 Ondestroy

--- WinProg2/Graphic Editor/Graphic Editor View.cpp
+++ WinProg2/Graphic Editor/Graphic Editor View.cpp
@@ -759,10 +759,15 @@ void CGraphicEditorView::OnKeyDown(UINT nChar, UINT nRepCnt, UINT nFlags)
 	// TODO: 여기에 메시지 처리기 코드를 추가 및/또는 기본값을 호출합니다.
 	switch (nChar)
 	{
-	case VK_ESCAPE:
+	case VK_ESCAPE://esc
 	case VK_CANCEL:
 	case VK_BACK:
 		cancelInsert();
+		break;
+	case VK_DELETE:
+		if (!m_CurrentFigures.IsEmpty()) {
+			OnDestroy(); // destroy 호출
+		}
 		break;
 	}
 	CView::OnKeyDown(nChar, nRepCnt, nFlags);
