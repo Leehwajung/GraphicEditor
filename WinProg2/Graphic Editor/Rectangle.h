9c541f7905521f921a118c53c9d6daa250c705c9
변준 <bjjang9407@naver.com>
2015-06-09 05:41:31 +0900

    CRectangle,CEllipse creating 구현 및 view에 연결

--- WinProg2/Graphic Editor/Rectangle.h
+++ WinProg2/Graphic Editor/Rectangle.h
@@ -98,7 +98,6 @@ public:
 	//		CreateFlag createFlag = FREECREATE: 생성 설정 플래그
 	RectF creating(IN Graphics& graphics, IN PointF startingPoint, IN PointF targetPoint, IN CreateFlag createFlag = FREECREATE);
 
-protected:
 	// 생성 그리기 (순수 가상)
 	// 생성 시에 보여줄 그리기
 	// - IN 매개변수
